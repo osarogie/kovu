5dc98a71f88ff34b588f403179dcf1290d0fbbcb
Emmanuel Nosakhare <nonreactiv@gmail.com>
2017-08-07 19:00:06 +0100

    Constants and colors

---
+++ src/constants.js
@@ -0,0 +1,9 @@
+module.exports = {
+  USER_ID: '@_USER_ID',
+  API_KEY: '@_API_KEY',
+  USER: '@_USER',
+  DARK: '@_DARK'
+  // CABLE_URL: 'ws://localhost:3000/_/cable',
+  // NEW_MESSAGES: 'new-messages',
+  // OLD_MESSAGES: 'old-messages',
+}
