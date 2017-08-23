85796e69026a9ca9d232c1abc50cc9e932cb1ee1
Emmanuel Nosakhare <nonreactiv@gmail.com>
2017-08-23 22:38:42 +0100

    Updated colors values

--- src/colors.js
+++ src/colors.js
@@ -1,14 +1,54 @@
-module.exports = [
-  // '#ee0979',
-  // '#ff6a00',
-  // '#41295a',
-  // '#734b6d'
-  '#627',
-  '#494',
-  '#725',
-  '#377',
-  '#478',
-  '#05f'
-]
+const colors = {
+  statusBar: '#000',
+  statusBarDark: '#262627',
+  toolbar: '#fff',
+  toolbarDark: '#262627',
+  toolbarTitleColor: '#000',
+  toolbarTitleColorDark: '#fff',
+  optionIcon: '#05f',
+  optionIconDark: '#fff',
+  container: '#fff',
+  containerDark: '#262627',
+  postList: '#fff',
+  postListDark: '#262627',
+  icon: '#05f',
+  iconDark: '#fff',
+  black: '#000',
+  blackDark: '#fff',
+  primary: '#333',
+  primaryDark: '#fff',
+  accent: '#05f',
+  // accentDark: '#fff',
 
-// module.exports = ['#C9D6FF','#E2E2E2','#d9a7c7','#78ffd6']
+  random(id = -1) {
+    const _colors = [
+      '#ee0979',
+      '#ff6a00',
+      '#41295a',
+      '#734b6d',
+      '#627',
+      '#494',
+      '#725',
+      '#377',
+      '#478',
+      '#05f',
+      '#C9D6FF',
+      '#E2E2E2',
+      '#d9a7c7',
+      '#78ffd6'
+    ]
+
+    if (id < 0) id = Math.floor(Math.random() * (_colors.length - 1))
+
+    return _colors[id % _colors.length]
+  }
+}
+
+module.exports = {
+  ...colors,
+  get(key, night_mode = false) {
+    return night_mode && colors[`${key}Dark`]
+      ? colors[`${key}Dark`]
+      : colors[key]
+  }
+}
