e386715ef5ad45ee494d715359d643d31689f986
Emmanuel Nosakhare <nonreactiv@gmail.com>
2017-08-23 22:30:24 +0100

    Rewrote App.js

--- src/App.js
+++ src/App.js
@@ -1,44 +1,42 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
  * @flow
  */
 
-import React, { Component } from 'react';
-import { AppRegistry, StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { connect, Provider } from 'react-redux'
+import { StatusBar, View } from 'react-native'
+import { Root } from './navigation'
+import { addNavigationHelpers } from 'react-navigation'
+import getStore from './store'
+import colors from './colors'
 
-export default class App extends Component {
+const store = getStore()
+const mapStateToProps = state => ({
+  nav: state.nav,
+  night_mode: state.night_mode
+})
+
+export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
+      <Provider store={store}>
+        <Main />
+      </Provider>
+    )
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF'
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5
-  }
-});
+const Main = connect(mapStateToProps)(({ dispatch, nav, night_mode }) =>
+  <View style={{ flex: 1 }}>
+    <StatusBar
+      backgroundColor={colors.get('statusBar', night_mode)}
+      barStyle="light-content"
+    />
+    <Root
+    // navigation={addNavigationHelpers({
+    //   dispatch: dispatch,
+    //   state: nav
+    // })}
+    />
+  </View>
+)
