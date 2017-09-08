1c685e61a4b53d76b3e1c5b53fe339f057e8488b
Emmanuel Nosakhare <nonreactiv@gmail.com>
2017-09-08 22:48:01 +0100

    iOS Js entry point

--- index.ios.js
+++ index.ios.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import { AppRegistry } from 'react-native';
-import App from './src/App';
-import codePush from 'react-native-code-push';
+// import './shim'
+import React from 'react'
+import { AppRegistry } from 'react-native'
+import App from './src/App'
+import codePush from 'react-native-code-push'
 
-AppRegistry.registerComponent('kovu', () => codePush(App));
+AppRegistry.registerComponent('kovu', () => codePush(App))
