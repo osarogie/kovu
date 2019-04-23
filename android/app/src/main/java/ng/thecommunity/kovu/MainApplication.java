9103abd5f7b2746dfbd8dac83f1786b10b9330fb
Emmanuel Nosakhare <nonreactiv@gmail.com>
2019-04-23 03:14:44 +0100

    Upgraded to react native 0.59 and react 16.8

--- android/app/src/main/java/ng/thecommunity/kovu/MainApplication.java
+++ android/app/src/main/java/ng/thecommunity/kovu/MainApplication.java
@@ -4,59 +4,44 @@ import android.app.Application;
 
 import com.crashlytics.android.Crashlytics;
 import com.facebook.react.ReactApplication;
-import com.learnium.RNDeviceInfo.RNDeviceInfo;
-import com.reactlibrary.googlesignin.RNGoogleSignInPackage;
+import com.github.alinz.reactnativewebviewbridge.WebViewBridgePackage;
 import com.react.rnspinkit.RNSpinkitPackage;
+import com.swmansion.reanimated.ReanimatedPackage;
 import com.reactnative.ivpusic.imagepicker.PickerPackage;
+import com.reactlibrary.googlesignin.RNGoogleSignInPackage;
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.reactnative.androidsdk.FBSDKPackage;
+import com.learnium.RNDeviceInfo.RNDeviceInfo;
+import com.microsoft.codepush.react.CodePush;
 import com.wix.RNCameraKit.RNCameraKitPackage;
+import com.cmcewen.blurview.BlurViewPackage;
 import com.airbnb.android.react.lottie.LottiePackage;
-
-import com.oblador.vectoricons.VectorIconsPackage;
 import com.microsoft.appcenter.reactnative.crashes.AppCenterReactNativeCrashesPackage;
 import com.microsoft.appcenter.reactnative.analytics.AppCenterReactNativeAnalyticsPackage;
 import com.microsoft.appcenter.reactnative.appcenter.AppCenterReactNativePackage;
-import com.cmcewen.blurview.BlurViewPackage;
-import com.reactnative.photoview.PhotoViewPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-import com.microsoft.codepush.react.CodePush;
-import com.github.alinz.reactnativewebviewbridge.WebViewBridgePackage;
 
 import com.facebook.CallbackManager;
 import com.facebook.FacebookSdk;
 import com.facebook.appevents.AppEventsLogger;
-
-// import io.realm.react.RealmReactPackage;
-
-import com.microsoft.codepush.react.ReactInstanceHolder;
-import com.oblador.vectoricons.VectorIconsPackage;
 
 import io.fabric.sdk.android.Fabric;
 import java.util.Arrays;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
-  private static CallbackManager mCallbackManager = CallbackManager.Factory.create();
 
-  protected static CallbackManager getCallbackManager() {
-    return mCallbackManager;
-  }
+  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
 
-  private class MyReactNativeHost extends ReactNativeHost implements ReactInstanceHolder{
-
-    MyReactNativeHost(Application application) {
-      super(application);
-    }
-
-    @Override
-    protected String getJSBundleFile() {
-      return CodePush.getJSBundleFile();
-    }
-
+        @Override
+        protected String getJSBundleFile() {
+        return CodePush.getJSBundleFile();
+        }
+    
     @Override
     public boolean getUseDeveloperSupport() {
       return BuildConfig.DEBUG;
@@ -65,27 +50,31 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-            new MainReactPackage(),
-            new RNDeviceInfo(),
-            new PhotoViewPackage(),
-            new LinearGradientPackage(),
+          new MainReactPackage(),
             new WebViewBridgePackage(),
             new RNSpinkitPackage(),
+            new ReanimatedPackage(),
             new PickerPackage(),
             new RNGoogleSignInPackage(),
-            new FBSDKPackage(mCallbackManager),
+            new RNGestureHandlerPackage(),
+            new FBSDKPackage(),
+            new RNDeviceInfo(),
+            new CodePush(BuildConfig.CODEPUSH_KEY, MainApplication.this, BuildConfig.DEBUG),
             new RNCameraKitPackage(),
             new BlurViewPackage(),
+            new LottiePackage(),
             new AppCenterReactNativeCrashesPackage(MainApplication.this, getResources().getString(R.string.appCenterCrashes_whenToSendCrashes)),
             new AppCenterReactNativeAnalyticsPackage(MainApplication.this, getResources().getString(R.string.appCenterAnalytics_whenToEnableAnalytics)),
             new AppCenterReactNativePackage(MainApplication.this),
-            new LottiePackage(),
-            new VectorIconsPackage(),
-            new CodePush(BuildConfig.CODEPUSH_KEY, MainApplication.this, BuildConfig.DEBUG)
+            new LinearGradientPackage()
       );
     }
-  }
-  private final MyReactNativeHost mReactNativeHost = new MyReactNativeHost(this);
+
+    @Override
+    protected String getJSMainModuleName() {
+      return "index";
+    }
+  };
 
   @Override
   public ReactNativeHost getReactNativeHost() {
@@ -94,7 +83,6 @@ public class MainApplication extends Application implements ReactApplication {
 
   @Override
   public void onCreate() {
-    CodePush.setReactInstanceHolder(mReactNativeHost);
     super.onCreate();
     Fabric.with(this, new Crashlytics());
     SoLoader.init(this, /* native exopackage */ false);
--- index.ios.js
+++
@@ -1,7 +0,0 @@
-// import './shim'
-import React from 'react'
-import { AppRegistry } from 'react-native'
-import App from './src/App'
-import codePush from 'react-native-code-push'
-
-AppRegistry.registerComponent('kovu', () => codePush(App))
