3e4134676b10bd5923cebdb635a5cb81655483f6
Emmanuel Nosakhare <nonreactiv@gmail.com>
2020-05-25 19:52:45 +0100

    Stop tracking /ios/Pods/

--- ios/Pods/Headers/Private/react-native-blur/BlurView.h
+++
@@ -1,1 +0,0 @@
-../../../../../node_modules/react-native-blur/ios/BlurView.h
--- ios/Pods/Target Support Files/Pods-kovu-tvOSTests/Pods-kovu-tvOSTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_kovu_tvOSTests : NSObject
-@end
-@implementation PodsDummy_Pods_kovu_tvOSTests
-@end
